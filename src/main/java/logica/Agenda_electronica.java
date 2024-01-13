1834c8586d154543d69dd16cf15baa8d9624a267
ramroman163 <ramroman163@gmail.com>
2024-01-13 00:42:54 -0300

    first commit

---
+++ src/main/java/logica/Agenda_electronica.java
@@ -0,0 +1,18 @@
+package logica;
+
+import igu.Principal;
+
+/**
+ *
+ * @author Roman
+ */
+public class Agenda_electronica {
+
+    public static void main(String[] args) {
+        Principal ventana = new Principal();
+        
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+       
+    }
+}
